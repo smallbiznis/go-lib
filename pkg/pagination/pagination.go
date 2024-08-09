9ad363bcb6236e0cd0025e043dba14c2d987e33d
Taufik Triantono <taufiktriantono4@gmail.com>
2024-08-09 22:59:50 +0700

    fix

--- pkg/pagination/pagination.go
+++ pkg/pagination/pagination.go
@@ -11,7 +11,11 @@ type Pagination struct {
 
 func (p Pagination) Paginate() func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
+		if p.PerPage > 0 {
+			db.Limit(p.PerPage)
+		}
+
 		offset := (p.Page - 1) * p.PerPage
-		return db.Offset(offset).Limit(p.PerPage)
+		return db.Offset(offset)
 	}
 }
