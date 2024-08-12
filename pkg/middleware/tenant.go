3d936aa5642fc47b0ef7c3e2ca517d2ffc6ba3e5
Taufik Triantono <taufiktriantono4@gmail.com>
2024-08-12 16:14:13 +0700

    fix: middleware tenant

--- pkg/middleware/tenant.go
+++ pkg/middleware/tenant.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"context"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,9 +9,8 @@ import (
 func Tenant() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
-		host := c.Request.Host
 
-		c.Request = c.Request.WithContext(context.WithValue(ctx, "tenant", strings.Split(host, ".")[0]))
+		c.Request = c.Request.WithContext(context.WithValue(ctx, "tenant", c.Request.Host))
 		c.Next()
 	}
 }
