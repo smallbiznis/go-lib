c6f9612aee22a1f3ab0623d85c0f00007e3e47ab
Taufik Triantono <taufiktriantono4@gmail.com>
2024-08-20 20:20:35 +0700

    fix

---
+++ pkg/logger/logger.go
@@ -0,0 +1,31 @@
+package logger
+
+import (
+	"github.com/smallbiznis/go-lib/pkg/env"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
+
+var (
+	NewZapLogger = fx.Module("zap.Logger", fx.Options(
+		fx.Provide(
+			InitLogger,
+		),
+	))
+)
+
+func InitLogger() (log *zap.Logger) {
+	fields := zap.Fields(
+		zap.String("service_name", env.Lookup("SERVICE_NAME", "example")),
+		zap.String("service_version", env.Lookup("SERVICE_VERSION", "1.0.0")),
+		zap.String("service_namespace", env.Lookup("SERVICE_NAMESPACE", "smallbiznis")),
+	)
+
+	log = zap.Must(zap.NewDevelopment(fields))
+	if env.Lookup("ENV", "development") == "production" {
+		log = zap.Must(zap.NewProduction(fields))
+		zap.ReplaceGlobals(log)
+	}
+
+	return log
+}
