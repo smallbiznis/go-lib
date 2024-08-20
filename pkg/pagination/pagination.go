9a72371bc505cfbc10afac798cf18a9157e2a884
Taufik Triantono <taufiktriantono4@gmail.com>
2024-08-20 20:31:36 +0700

    fix: change per_page with size

--- pkg/pagination/pagination.go
+++ pkg/pagination/pagination.go
@@ -3,7 +3,7 @@ package pagination
 import "gorm.io/gorm"
 
 type Pagination struct {
-	PerPage int    `form:"per_page" validate:"min=1,max=250"`
+	Size    int    `form:"size" validate:"min=1,max=250"`
 	Page    int    `form:"page" validate:"min=1"`
 	SortBy  string `form:"sort_by"`
 	OrderBy string `form:"order_by"`
@@ -11,11 +11,11 @@ type Pagination struct {
 
 func (p Pagination) Paginate() func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
-		if p.PerPage > 0 {
-			db.Limit(p.PerPage)
+		if p.Size > 0 {
+			db.Limit(p.Size)
 		}
 
-		offset := (p.Page - 1) * p.PerPage
+		offset := (p.Page - 1) * p.Size
 		return db.Offset(offset)
 	}
 }
